52ae3b1ac7036969a6fd404ba4d40d9e9fc647d6
unknown <AhmedElgafy>
2023-04-02 14:42:26 +0200

    implment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,65 @@
 // import { isFile } from '@babel/types'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
-const ToDoItems=()=>{
+const ToDoItem=(props)=>{
+  useEffect(()=>console.log("changed"),props.itemsLeft)
   return(
+    <>
+      <div className="toDoCreator toDoItem">
+          <div className='insideToDoItem'>
+            <div className='circle'></div>
+            <h4>{props.data}</h4>
+          </div>
+          <img src="../icon-cross.svg" alt="" />
+        </div>
+    </>
+  )
+}
 
-    <div className="toDoCreator toDoItem">
-    <div className='insideToDoItem'>
-      <div className='circle'></div>
-      <h4>Create a new todo...</h4>
-    </div>
-    <img src="../icon-cross.svg" alt="" />
-  </div>
+
+
+const ToDoItems=(props)=>{
+  const [numOfItems,setNumOfItems]=useState(1)
+  useEffect(()=>console.log(55),[props.itemsLeft])
+  // console.log(props.dataList)
+  
+  return(
+      <>
+        {props.dataList?props.dataList:null}
+
+        <div className="toDoCreator status sm-font">
+          <p>{props.itemsLeft} items left</p>
+          <div className="inside">
+            <h4 className='active'>All</h4>
+            <h4>Active</h4>
+            <h4>Completed</h4>
+          </div>
+          <p>Clear Completed</p>
+        </div>
+      </>
+  
 )
 }
-console.log("hi")
+// console.log("hi")
+
+
+
+
 
 function App() {
-  const [toDoList, setToDoList] = useState(["Jog around the park 3*","10 minutes meditation","Read for 1 hour"])
+  const [toDoList, setToDoList] = useState([])
+  const [itemsLeft,setItemsLeft]=useState(0)
+  
+  
+  const doIt=(e)=>{
+    let data=e.target.value
+    toDoList.push(<ToDoItem data={data} key={data} />)
+    setItemsLeft((e)=>e=e+1)
+    console.log(toDoList)
+  }
+  
+  useEffect(()=>console.log(itemsLeft),[itemsLeft]) 
 
   return (
     <>
@@ -29,20 +71,12 @@ function App() {
       </div>
       <div className="toDoCreator">
         <div className='circle'></div>
-        <h4>Create a new todo...</h4>
+        <input type="text" onKeyDown={(e)=>e.key=="Enter"?doIt(e):null} placeholder='Create a new todo...' />
+        {/* <h4>Create a new todo...</h4> */}
       </div>
 
+      <ToDoItems dataList={toDoList} itemsLeft={itemsLeft}/>
 
-      {toDoList?<ToDoItems listData={toDoList}/>:null}
-
-
-      <div className="status toDoCreator">
-        <div className="inside">
-          <h4 className='active'>All</h4>
-          <h4>Active</h4>
-          <h4>Completed</h4>
-        </div>
-      </div>
     </div>
     </>
   )
