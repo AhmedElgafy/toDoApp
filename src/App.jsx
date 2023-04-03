f47c274ce76334eb3fe4c68f3c36496013ae5bc3
unknown <AhmedElgafy>
2023-04-03 15:56:51 +0200

    some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,38 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
 import './App.css'
+// const array = [2, 5, 9];
+
+
+// const index = array.indexOf(2);
+// // if (index > -1) { // only splice array when item is found
+//   array.splice(index, 1); // 2nd parameter means remove one item only
+// // }
+
 const ToDoItem=(props)=>{
-  // useEffect(()=>console.log("changed"),props.itemsLeft)
   const handelExit=(e)=>{
-    console.log(e.key)
+    props.setItemsLeft(e=>e=e-1)
+    // console.log(e.target.id)
+    props.removeItemWithId(e.target.id)
+  }
+  const makeCheck=(e)=>{
+    
+    e.target.style.backgroundImage="no-repeat url(../icon-check.svg)"
+    e.target.style.backgroundColor="black"
+    e.target.style.backgroundPosition="center"
+
+    console.log(e.target.style)
   }
   return(
     <>
-      <div className="toDoCreator toDoItem">
+      <div className="toDoCreator toDoItem" id={props.data}>
           <div className='insideToDoItem'>
-            <div className='circle'></div>
+            <div className='circle 'onClick={makeCheck}></div>
             <h4>{props.data}</h4>
           </div>
-          <img src="../icon-cross.svg" key={props.data} alt="" 
-          onClick={(e)=>handelExit(e)}/>
+          <img key2={props.key2} src="../icon-cross.svg" id={props.key2} alt="" 
+          onClick={(e)=>handelExit(e)}
+          />
         </div>
     </>
   )
@@ -25,12 +43,11 @@ const ToDoItem=(props)=>{
 
 const ToDoItems=(props)=>{
   const [numOfItems,setNumOfItems]=useState(1)
-  console.log(props.dataList)
-  
+  // useEffect(()=>null,[numOfItems])
+  const clearzList=()=>{props.setToDoList([]);props.setItemsLeft(0)}
   return(
       <>
         {props.dataList.map((element)=>element)}
-
         <div className="toDoCreator status sm-font">
           <p>{props.itemsLeft} items left</p>
           <div className="inside">
@@ -38,13 +55,12 @@ const ToDoItems=(props)=>{
             <h4>Active</h4>
             <h4>Completed</h4>
           </div>
-          <p>Clear Completed</p>
+          <p onClick={clearzList}>Clear Completed</p>
         </div>
       </>
   
 )
 }
-// console.log("hi")
 
 
 
@@ -53,16 +69,31 @@ const ToDoItems=(props)=>{
 function App() {
   const [toDoList, setToDoList] = useState([])
   const [itemsLeft,setItemsLeft]=useState(0)
+  const [key,setKey]=useState(0)
+
+  const removeItemWithId=(key)=>{
+    //if key == element ---> get the index of element ------> and remove it 
+    for(let element of toDoList){
+        let index=toDoList.indexOf(element)
+        // console.log(key)
+        if(element.props.key2==key){
+          toDoList.splice(index,1)
+      }
+    }
+  }
   
   
   const doIt=(e)=>{
     let data=e.target.value
-    toDoList.push(<ToDoItem data={data} key={data} />)
-    setItemsLeft((e)=>e=e+1)
-    console.log(toDoList)
+    if(data){
+      let key2 =key
+      toDoList.push(<ToDoItem key2={key2} setItemsLeft={setItemsLeft} removeItemWithId={removeItemWithId} data={data} id={data}  />)
+      setKey(e=>e=e+1)
+      setItemsLeft((e)=>e=e+1)
+      e.target.value=""
+    }
   }
-  
-  // useEffect(()=>console.log(itemsLeft),[itemsLeft]) 
+
 
   return (
     <>
@@ -78,7 +109,7 @@ function App() {
         {/* <h4>Create a new todo...</h4> */}
       </div>
 
-      <ToDoItems dataList={toDoList} itemsLeft={itemsLeft}/>
+      <ToDoItems setToDoList={setToDoList} dataList={toDoList} setItemsLeft={setItemsLeft} itemsLeft={itemsLeft}/>
 
     </div>
     </>
